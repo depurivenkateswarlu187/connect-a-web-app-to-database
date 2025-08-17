83bd2752f0ecd9107a5f3f644fc2f4f54c4486d8
depurivenkateswarlu187 <vv1833127@gmail.com>
2025-08-17 14:14:09 +0530

    Create database.php

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_SERVER','nextwork-db-cluster.cluster-czy2u84igsez.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'n3xtw0rk');
+define('DB_DATABASE', 'sample');
+php?>
